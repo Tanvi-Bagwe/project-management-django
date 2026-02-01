4aa969baa447f0efcb5b05868ab70ac7376e3058
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-02-01 18:03:41 +0000

    database connection added

--- core/settings.py
+++ core/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',  # The one you reset via pgAdmin
+        'HOST': '127.0.0.1',
+        'PORT': '5454',
+        'OPTIONS': {
+            'options': '-c search_path=project_management,public'
+        },
     }
 }
 
