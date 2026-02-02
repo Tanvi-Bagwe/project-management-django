72e90b500955408a3008af2f78c895bf5cf07012
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-02-02 12:16:56 +0000

    first route added

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from profile import Profile
 
-# Create your views here.
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
+
+def register(request):
+    if request.method == 'POST':
+        user = User.objects.create_user(
+            username=request.POST['username'],
+            email=request.POST['email'],
+            password=request.POST['password'],
+            first_name=request.POST.get('first_name'),
+            last_name=request.POST.get('last_name'),
+        )
+
+        Profile.objects.create(
+            user=user,
+            phone=request.POST.get('phone'),
+            role=request.POST.get('role')
+        )
+
+        return redirect('login')
+
+    return render(request, 'accounts/register.html')
