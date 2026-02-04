2a018a194c07b5037a0bdc56c00bd1a7a0e538fd
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-02-04 22:10:56 +0000

    models added for project flow

--- projects/models.py
+++ projects/models.py
@@ -1,1 +1,28 @@
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class ProjectStatus(models.Model):
+    id = models.AutoField(primary_key=True)
+    code = models.CharField(max_length=30)
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        managed = False
+        db_table = "project_status"
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    status = models.ForeignKey(ProjectStatus, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = "projects"
