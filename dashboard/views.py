72078ffab64e9e3fbfa90e5a3ca012785c2cada8
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-02-04 21:23:27 +0000

    role based dashboard configured - placeholder

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from accounts.models.profile import Profile
+
 
 @login_required(login_url="login")
 def dashboard(request):
-    return render(request, "dashboard/index.html")
+    profile = Profile.objects.get(user=request.user)
+
+    context = {
+        "role": profile.role
+    }
+
+    return render(request, "dashboard/index.html", context)
