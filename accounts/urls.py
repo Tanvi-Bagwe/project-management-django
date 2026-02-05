47878c9763ec21c0274e8a4830c8e6014db190d0
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-02-05 20:09:19 +0000

    urls moved to accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from .views import register, register_submit, login_submit, login_page, logout_view
@@ -8,4 +9,28 @@ urlpatterns = [
     path("login/", login_page, name="login"),
     path("login/submit/", login_submit, name="login_submit"),
     path("logout/", logout_view, name="logout"),
+
+    path("password-reset/",
+         auth_views.PasswordResetView.as_view(
+             template_name="accounts/password_reset.html"
+         ),
+         name="password_reset"),
+
+    path("password-reset/done/",
+         auth_views.PasswordResetDoneView.as_view(
+             template_name="accounts/password_reset_done.html"
+         ),
+         name="password_reset_done"),
+
+    path("reset/<uidb64>/<token>/",
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name="accounts/password_reset_confirm.html"
+         ),
+         name="password_reset_confirm"),
+
+    path("reset/done/",
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name="accounts/password_reset_complete.html"
+         ),
+         name="password_reset_complete"),
 ]
