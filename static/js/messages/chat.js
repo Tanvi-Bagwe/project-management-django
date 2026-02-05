afd4d8ae4dd71bb7a2565dce8fac736f454b59e3
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-02-05 23:01:56 +0000

    messaging flow intitated

---
+++ static/js/messages/chat.js
@@ -0,0 +1,44 @@
+let currentConv = null;
+let poller = null;
+
+function openChat(convId) {
+    currentConv = convId;
+    loadMessages();
+
+    if (poller) clearInterval(poller);
+    poller = setInterval(loadMessages, 30000);
+}
+
+function loadMessages() {
+    fetch(`/messages/${currentConv}/`)
+        .then(res => res.json())
+        .then(data => {
+            const box = document.getElementById("chatBox");
+            box.innerHTML = "";
+
+            data.messages.forEach(m => {
+                const div = document.createElement("div");
+                div.className = "msg " + (m.is_me ? "me" : "other");
+                div.innerText = m.content;
+                box.appendChild(div);
+            });
+
+            box.scrollTop = box.scrollHeight;
+        });
+}
+
+function sendMsg() {
+    const input = document.getElementById("msgInput");
+
+    fetch(`/messages/${currentConv}/send/`, {
+        method: "POST",
+        headers: {
+            "X-CSRFToken":
+            document.querySelector('[name=csrfmiddlewaretoken]').value
+        },
+        body: new URLSearchParams({content: input.value})
+    }).then(() => {
+        input.value = "";
+        loadMessages();
+    });
+}
