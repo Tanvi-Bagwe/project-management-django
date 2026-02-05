e8e122e6543ebf874fd538a896b93c1c28569511
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-02-05 17:40:03 +0000

    url for tasks

---
+++ tasks/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import task_detail, update_task_status
+
+urlpatterns = [
+    path("tasks/<int:task_id>/", task_detail, name="task_detail"),
+    path("tasks/<int:task_id>/status/", update_task_status, name="task_status_update"),
+]
