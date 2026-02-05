4910387abaabdbe5c1c217f553283b78a16b26cc
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-02-05 23:02:18 +0000

    models and views added

--- messaging/models.py
+++ messaging/models.py
@@ -1,1 +1,26 @@
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Conversation(models.Model):
+    id = models.AutoField(primary_key=True)
+    user1 = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name="conv_user1")
+    user2 = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name="conv_user2")
+    created_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = "conversations"
+        unique_together = ("user1", "user2")
+
+
+class Message(models.Model):
+    id = models.AutoField(primary_key=True)
+    conversation = models.ForeignKey(Conversation, on_delete=models.DO_NOTHING)
+    sender = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    content = models.TextField()
+    created_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = "messages"
