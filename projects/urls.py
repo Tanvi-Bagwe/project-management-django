df7fae9c0533294759859a6dad78391cae4e6b4b
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-02-05 21:36:14 +0000

    project creation completed

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from projects.views import project_detail
+from projects.views import project_detail, create_project
 
 urlpatterns = [
     path("projects/<int:project_id>/", project_detail, name="project_detail"),
+
+    path("projects/create/", create_project, name="create_project"),
 
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
 
 from projects.models import Project
@@ -19,3 +20,22 @@ def project_detail(request, project_id):
         "statuses": TaskStatus.objects.all(),
         "priorities": TaskPriority.objects.all()
     })
+
+
+@login_required
+def create_project(request):
+    if request.user.profile.role != "manager":
+        return JsonResponse({"error": "Unauthorized"}, status=403)
+
+    if request.method == "POST":
+        project = Project.objects.create(
+            name=request.POST["name"],
+            description=request.POST["description"],
+            created_by=request.user,
+            status_id=1  # active
+        )
+        return JsonResponse({
+            "success": True,
+            "project_id": project.id
+        })
+    return None
