1ac098a5c3d492009a2a5ec8a8755d3a2b80575d
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-02-05 20:09:46 +0000

    task create view added

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,32 +1,11 @@
-from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from .views import task_detail, update_task_status
+from .views import task_detail, update_task_status, create_task
 
 urlpatterns = [
     path("tasks/<int:task_id>/", task_detail, name="task_detail"),
     path("tasks/<int:task_id>/status/", update_task_status, name="task_status_update"),
-    path("password-reset/",
-         auth_views.PasswordResetView.as_view(
-             template_name="accounts/password_reset.html"
-         ),
-         name="password_reset"),
 
-    path("password-reset/done/",
-         auth_views.PasswordResetDoneView.as_view(
-             template_name="accounts/password_reset_done.html"
-         ),
-         name="password_reset_done"),
+    path("tasks/create/", create_task, name="create_task"),
 
-    path("reset/<uidb64>/<token>/",
-         auth_views.PasswordResetConfirmView.as_view(
-             template_name="accounts/password_reset_confirm.html"
-         ),
-         name="password_reset_confirm"),
-
-    path("reset/done/",
-         auth_views.PasswordResetCompleteView.as_view(
-             template_name="accounts/password_reset_complete.html"
-         ),
-         name="password_reset_complete"),
 ]
