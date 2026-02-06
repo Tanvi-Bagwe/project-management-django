2c1e7534727aac8ae60e6ab1f0540296d0cb204a
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-02-06 23:32:54 +0000

    landing page added

--- core/urls.py
+++ core/urls.py
@@ -1,15 +1,16 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.urls import path, include
 
 
 def root_redirect(request):
     if request.user.is_authenticated:
-        return redirect('dashboard')
-    return redirect('login')
+        return redirect("dashboard")
+
+    return render(request, "landing.html")
 
 
 urlpatterns = [
-    path('', root_redirect),
+    path("", root_redirect, name="landing"),
     path('', include('accounts.urls')),
     path('', include('dashboard.urls')),
     path('', include('tasks.urls')),
