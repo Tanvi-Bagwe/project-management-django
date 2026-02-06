1c79bef969d996262dc29e836737b29bf7ed1333
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-02-06 19:40:31 +0000

    admin urls

--- projects/urls.py
+++ projects/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from projects.views import project_detail, create_project
+from projects.views import project_detail, create_project, admin_projects
 
 urlpatterns = [
     path("projects/<int:project_id>/", project_detail, name="project_detail"),
 
     path("projects/create/", create_project, name="create_project"),
+
+    path("projects/admin", admin_projects, name="admin_projects"),
 
 ]
